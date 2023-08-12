86d9e45f72b6fb84327ceac95689c164c34a04db
mastersemsk <57392843+mastersemsk@users.noreply.github.com>
2023-08-13 00:04:57 +0600

    Update Send

--- back/Content/PHPMailer/SendMail.php
+++ back/Content/PHPMailer/SendMail.php
@@ -18,18 +18,18 @@ class SendMail
      //Server settings
      //$mail->SMTPDebug = SMTP::DEBUG_SERVER;                      //Enable verbose debug output
      $mail->isSMTP();                                            //Send using SMTP
-     $mail->Host       = 'smtp.yandex.com';                     //Set the SMTP server to send through
+     $mail->Host       = '';                     //Set the SMTP server to send through
      $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
-     $mail->Username   = 'help@wm-semsk.kz';                     //SMTP username
-     $mail->Password   = 'hmxvtsrlpirnondk';                     //SMTP password
+     $mail->Username   = '';                     //SMTP username
+     $mail->Password   = '';                     //SMTP password
      $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;            //Enable implicit TLS encryption
      $mail->Port       = 465;                                    //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
 
      //Recipients 
-     $mail->setFrom('help@wm-semsk.kz', 'WMsemsk');         // Адрес самой почты и имя отправителя
+     $mail->setFrom('');         // Адрес самой почты и имя отправителя
      $mail->addAddress($pochta, $imia);                     // Получатель письма
      //$mail->addAddress('ellen@example.com');               //Name is optional
-     $mail->addReplyTo('help@wm-semsk.kz', 'Information');
+     $mail->addReplyTo('');
      //$mail->addCC('cc@example.com');
      //$mail->addBCC('bcc@example.com');
 
