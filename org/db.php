2496000ea2c7790959882787cc5a0a3f18482d1f
Ivan Romanenko <romanenko@inbox.ru>
2025-01-20 09:10:08 +0700

    dbs

--- org/db.php
+++ org/db.php
@@ -1,26 +1,26 @@
 <?php
 class FuncSQL {
 
-const HOST = 'localhost';
-const USER = 'root';
-const PASS = 'root';
-const DB = 'baza';
+private const HOST = 'localhost';
+private const USER = 'root';
+private const PASS = 'root';
+private const DB = 'baza';
 
 private static $db;
 private $result;
 	
 public function __construct() {
 	 error_reporting(0); mysqli_report(MYSQLI_REPORT_OFF);
-	 if (!$this->db)
-	 $this->db = new mysqli(self::HOST, self::USER, self::PASS, self::DB, 3306, null);
+	 if (!self::$db)
+	 self::$db = new mysqli(self::HOST, self::USER, self::PASS, self::DB, 3306, null);
 	   if (mysqli_connect_errno()) {
           throw new RuntimeException('ошибка соединения: ' . mysqli_connect_error());
         }
-	 mysqli_set_charset($this->db, "utf8mb4");
+	 mysqli_set_charset(self::$db, "utf8mb4");
 	}
 public function QUERY($sql) {
 	$result = null;
-	$result = mysqli_query($this->db, $sql) or die(mysqli_error($this->db));
+	$result = mysqli_query(self::$db, $sql) or die(mysqli_error(self::$db));
 	return $this->result = $result;
     }
 public function NumRows() {
@@ -29,7 +29,7 @@ public function NumRows() {
 	 return $rows;
 	}
 public function AffectRows() {
-	 $rows = mysqli_affected_rows($this->db);
+	 $rows = mysqli_affected_rows(self::$db);
 	 if(!isset($rows)) die();
 	  return $rows;
 	}
@@ -49,22 +49,22 @@ public function ArrayRows() {
     }
   
 public function Zakaz() {
-	 return mysqli_insert_id($this->db); //номер заказа
+	 return mysqli_insert_id(self::$db); //номер заказа
     }
 
 public function FreeRes() {
     mysqli_free_result($this->result);
     }
 public function Close() {	
-	mysqli_close($this->db);
+	mysqli_close(self::$db);
 	}
 public function newTransaction() {
-	 return mysqli_begin_transaction($this->db); //начало транзакции
+	 return mysqli_begin_transaction(self::$db); //начало транзакции
     }
 public function closeСommit() {
-	 return mysqli_commit($this->db); //конец успешной транзакции
+	 return mysqli_commit(self::$db); //конец успешной транзакции
     }
 public function closeRollback() {
-	 return mysqli_rollback($this->db); //возврат не успешной транзакции
+	 return mysqli_rollback(self::$db); //возврат не успешной транзакции
     }
 }
